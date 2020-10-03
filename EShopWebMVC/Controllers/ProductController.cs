225a835fd3f8da0366774666d1d6a5f15f47a6db
Le Xuan Thang <lxthanghy@gmail.com>
2020-10-03 09:01:04 +0700

    Add project files.

---
+++ EShopWebMVC/Controllers/ProductController.cs
@@ -0,0 +1,119 @@
+﻿using EShopWebMVC.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EShopWebMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly string BASE_URL = "http://localhost:54896/api/Product/";
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Product> products = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BASE_URL);
+                var getTask = client.GetAsync("Get");
+                getTask.Wait();
+                var result = getTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    string data = result.Content.ReadAsStringAsync().Result;
+                    products = JsonConvert.DeserializeObject<List<Product>>(data);
+                }
+            }
+            return View(products);
+        }
+        public ActionResult Create(Product p)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BASE_URL);
+                string data = JsonConvert.SerializeObject(p);
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                var postTask = client.PostAsync("AddProduct", content);
+                postTask.Wait();
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View();
+        }
+        public ActionResult Edit(int id)
+        {
+            Product product = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BASE_URL);
+                var getTask = client.GetAsync("Get/" + id);
+                getTask.Wait();
+                var result = getTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    string data = result.Content.ReadAsStringAsync().Result;
+                    product = JsonConvert.DeserializeObject<Product>(data);
+                }
+            }
+            return View("Edit", product);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product p)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BASE_URL);
+                string data = JsonConvert.SerializeObject(p);
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                var putTask = client.PutAsync("Put/" + p.ProductID, content);
+                putTask.Wait();
+                var result = putTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View();
+        }
+        public ActionResult Delete(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BASE_URL);
+                var deleteTask = client.DeleteAsync("Delete/" + id);
+                deleteTask.Wait();
+                var result = deleteTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(int id)
+        {
+            Product product = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BASE_URL);
+                var getTask = client.GetAsync("Get/" + id);
+                getTask.Wait();
+                var result = getTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    string data = result.Content.ReadAsStringAsync().Result;
+                    product = JsonConvert.DeserializeObject<Product>(data);
+                }
+            }
+            return View("Details", product);
+        }
+    }
+}
---
+++ EShopWebMVC/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EShopWebMVC.Models
+{
+    public class Product
+    {
+        private int productID;
+        private string productName;
+        private int categoryID;
+        private int unitPrice;
+        private int quantity;
+        public int ProductID { get => productID; set => productID = value; }
+        public string ProductName { get => productName; set => productName = value; }
+        public int CategoryID { get => categoryID; set => categoryID = value; }
+        public int UnitPrice { get => unitPrice; set => unitPrice = value; }
+        public int Quantity { get => quantity; set => quantity = value; }
+    }
+}
